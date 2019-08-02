44833a6cae40933e35a5ba7ea68430f82fefab85
akupathny madhavi <akupathny.madhavi@publicissapient.com>
2019-08-02 17:20:09 +0530

    react-app

---
+++ react app for git/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/header';
+import Sidebar from './components/sidebar';
+import Footer from './components/footer'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Sidebar/>
+      <Footer/>
+    </div>
+    
+  );
+}
+
+export default App;
+
+
+
+
---
+++ react app for git/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer (){
+    return(
+        <div>
+            <span>this is a Footer component</span>
+        </div>
+    )
+
+}
+
+export default Footer;
